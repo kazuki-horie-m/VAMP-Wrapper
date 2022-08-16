54ee44c815ef84d535d3122128acfffd4ef90a68
Kazuki Horie <1ao.keld.b@gmail.com>
2022-08-16 18:32:03 +0900

    init

---
+++ VAMP-Wrapper/VAMP-Wrapper/VAMP_Wrapper.h
@@ -0,0 +1,18 @@
+//
+//  VAMP_Wrapper.h
+//  VAMP-Wrapper
+//
+//  Created by Kazuki Horie on 2022/08/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VAMP_Wrapper.
+FOUNDATION_EXPORT double VAMP_WrapperVersionNumber;
+
+//! Project version string for VAMP_Wrapper.
+FOUNDATION_EXPORT const unsigned char VAMP_WrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VAMP_Wrapper/PublicHeader.h>
+
+
